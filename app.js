20812e78c5e4c92c4d867245edab2310d1a82e2f
kenjones21 <cubsfan@udel.edu>
2015-03-19 23:58:07 -0400

    Changed port to 3200

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ io.sockets.on("connection", function(socket) {
 });
 
 // Start the server (taken from Andy which is taken from Cloud9)
-server.listen(process.env.PORT || 3100, process.env.IP || "0.0.0.0", function() {
+server.listen(process.env.PORT || 3200, process.env.IP || "0.0.0.0", function() {
   var address = server.address();
   console.log("Server is now started on ", address.address + ":" + address.port);
 });
