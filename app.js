9c23face1ec6ec30f647e0357cfe8cf843259557
kenjones21 <cubsfan@udel.edu>
2015-03-19 23:56:53 -0400

    Created functional YQL App

---
+++ app.js
@@ -0,0 +1,69 @@
+// Import all necessary NodeJS modules
+var YQL = require('yql');
+    http = require('http');
+    path = require('path');
+    socketio = require('socket.io');
+    express = require('express');
+    colors = require("colors/safe");
+
+    // Start express, the server, and socket.io
+var router = express();
+    server = http.createServer(router);
+    io = socketio.listen(server);
+
+// Example #1 - Param binding
+new YQL.exec('select * from yahoo.finance.quote where symbol in ("YHOO","AAPL","GOOG","MSFT")', function(response) {
+
+  if (response.error) {
+    console.log("Example #1... Error: " + response.error.description);
+  }
+  else {
+        var symbol  = response.query.results.quote[0].symbol;
+        console.log("First symbol is " + symbol);
+  }
+});
+
+// Set the static file path to the public directory
+router.use(express.static(path.resolve(__dirname, "public")));
+
+// Catch anything that tries to connect to the server and serve up the single
+// page app located at public/index.html
+router.get("/*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "public/html/index.html"));
+});
+
+// Catch anything that might want to see all of the components that way in case 
+// we need to change the above route this still prvents people from peeking
+router.get("/components/*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
+//*** SOCKET IO STUFF ***//
+// Begin the socket.io logic
+// connection event is called for every new client thus this code occurs once for
+// each client
+io.sockets.on("connection", function(socket) {
+  console.log("New client from " + colors.green(socket.request.connection.remoteAddress));
+
+  socket.on("stockSearch", function(symbol){
+    console.log (colors.green(socket.request.connection.remoteAddress) + " wants stock " + symbol);
+    new YQL.exec('select * from yahoo.finance.quote where symbol in ("'+symbol+'")', function(response) {
+
+      if (response.error) {
+        console.log("Stock Search Error: " + response.error.description);
+      }
+      else {
+        //console.log(response.query.results);
+        var price  = response.query.results.quote.LastTradePriceOnly;
+        console.log("Requested stock's price is " + price);
+        socket.emit("stockFound", price);
+      }
+    });
+  });
+});
+
+// Start the server (taken from Andy which is taken from Cloud9)
+server.listen(process.env.PORT || 3100, process.env.IP || "0.0.0.0", function() {
+  var address = server.address();
+  console.log("Server is now started on ", address.address + ":" + address.port);
+});
---
+++ public/js/yqlTest.js
@@ -0,0 +1,38 @@
+// Create the angular app and inject the ngRoute module
+var app = angular.module("yqlTest", ["ngRoute"]);
+
+// Setup the routes used for the singlepage app
+app.config(["$routeProvider", "$locationProvider", 
+  function($routeProvider, $locationProvider) {
+    $routeProvider.when("/home", {
+      templateUrl: "/html/home.html",
+      controller: "HomeController"
+    }).otherwise({
+      redirectTo: "/home"
+    });
+}]);
+
+// Connect to the server using socket.io
+var socket = io.connect();
+var symbol = "";
+
+// This controller controls the Home screen
+app.controller("HomeController", ["$scope", "$location",
+  function($scope, $location) {
+
+    // Function called when a player wishes to join a game
+    $scope.getData = function() {
+      symbol = $scope.stockName;
+      console.log("Clicked the button");
+      //console.log("Joining a game as " + userProps.getUser().name);
+      socket.emit("stockSearch", symbol);
+    };
+
+    socket.on("stockFound", function(price) {
+      //console.log(userInfo.gameID);
+      console.log("The price of this stock is " + price);
+      $scope.price = price;
+      $scope.$apply();
+    });
+  }
+]);
